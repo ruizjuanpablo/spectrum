d244a1f39bba86b4260408b259213f08cec44dc3
Juan Pablo Ruiz <juanpablo.ruiz@tii.ae>
2022-10-31 12:46:21 +0000

    displayvm: add 500mb free space to displayvm
    Also, removed read only features to rootfs for displayvm
    
    Signed-off-by: Juan Pablo Ruiz <juanpablo.ruiz@tii.ae>

--- host/initramfs/extfs.nix
+++ host/initramfs/extfs.nix
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2021-2022 Alyssa Ross <hi@alyssa.is>
 # SPDX-License-Identifier: MIT
 
-{ pkgs, runCommand, tar2ext4 }:
+{ pkgs, runCommand, tar2ext4, e2fsprogs}:
 
 let
   appvm-display = import ../../vm/app/display {
@@ -11,7 +11,7 @@ let
 in
 
 runCommand "ext.ext4" {
-  nativeBuildInputs = [ tar2ext4 ];
+  nativeBuildInputs = [ tar2ext4 e2fsprogs];
 } ''
   mkdir svc
 
@@ -20,4 +20,7 @@ runCommand "ext.ext4" {
 
   tar -cf ext.tar svc
   tar2ext4 -i ext.tar -o $out
+	dd if=/dev/zero of=$out bs=1MiB count=100 conv=notrunc oflag=append
+	resize2fs $out
+	tune2fs -O ^read-only $out
 ''
